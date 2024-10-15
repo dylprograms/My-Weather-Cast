0ed2b4fd1aed47091afac2cd430f61e94a453a5a
Black Magic <dlyoungworking@gmail.com>
2024-10-14 23:32:31 -0700

    added api routes

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,20 +1,41 @@
 import { Router, type Request, type Response } from 'express';
+import { get } from 'http';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import HistoryService from '../../service/historyService.js'; 
+import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req: Request, res: Response) => {
+router.post('/weather', async (req: Request, res: Response) => {
+  const { city } = req.body;
+  const weatherService = new WeatherService();
+  const historyService = new HistoryService();
+  const coordinates = await weatherService.fetchAndDestructureLocationData(city);
+  const weatherData = await weatherService.fetchWeatherData(coordinates);
+  await historyService.addCity(city);
+  res.json(weatherData);
   // TODO: GET weather data from city name
   // TODO: save city to search history
 });
 
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+router.get('/history', async (req: Request, res: Response) => {
+  req;
+  const historyService = new HistoryService();
+  const cities = await historyService.getCities();
+  res.json(cities);
+});
+
+
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+router.delete('/history/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const historyService = new HistoryService();
+  await historyService.removeCity(id);
+  res.json({ message: 'City removed from search history' });
+});
+
 
 export default router;
--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -2,31 +2,104 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
+interface Coordinates {
+  lat: number;
+  lon: number;
+}
 
-// TODO:
- Define a class for the Weather object
+// TODO: Define a class for the Weather object
+class Weather {
+  city: string;
+  temperature: number;
+  description: string;
+  date?: string;
+  constructor(city: string, temperature: number, description: string, date?: string) {
+    this.city = city;
+    this.temperature = temperature;
+    this.description = description;
+    this.date = date;
+  }
 
-// TODO: Complete the WeatherService class
+// TODO: Complete the WeatherService class 
+
 class WeatherService {
   // TODO: Define the baseURL, API key, and city name properties
+  private baseURL: string;
+  private apiKey: string;
+  private cityName: string;
+  constructor() {
+    this.baseURL = 'https://api.openweathermap.org/data/2.5/weather';
+    this.apiKey = process.env.WEATHER_API_KEY || '';
+    this.cityName = '';
+  }
   // TODO: Create fetchLocationData method
   // private async fetchLocationData(query: string) {}
+  private fetchLocationData(query: string) {
+    const response = await fetch(query);
+    const data = await response.json();
+    return data;
+  }
   // TODO: Create destructureLocationData method
   // private destructureLocationData(locationData: Coordinates): Coordinates {}
+  private destructureLocationData(locationData: Coordinates) {
+    const { lat, lon } = locationData;
+    return { lat, lon };
+  }
   // TODO: Create buildGeocodeQuery method
   // private buildGeocodeQuery(): string {}
+ private buildGeocodeQuery() {
+    const query = `${this.baseURL}?q=${this.cityName}&appid=${this.apiKey}`;
+    return query;
+  }
   // TODO: Create buildWeatherQuery method
   // private buildWeatherQuery(coordinates: Coordinates): string {}
+  private buildWeatherQuery(coordinates: Coordinates) {
+    const query = `${this.baseURL}?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${this.apiKey}`;
+    return query;
+  } 
   // TODO: Create fetchAndDestructureLocationData method
   // private async fetchAndDestructureLocationData() {}
+  private async fetchAndDestructureLocationData() {
+    const query = this.buildGeocodeQuery();
+    const locationData = this.fetchLocationData(query);
+    const coordinates = this.destructureLocationData(locationData);
+    return coordinates;
+  }
   // TODO: Create fetchWeatherData method
   // private async fetchWeatherData(coordinates: Coordinates) {}
+  private async fetchWeatherData(coordinates: Coordinates) {
+    const query = this.buildWeatherQuery(coordinates);
+    const weatherData = this.fetchLocationData(query);
+    return weatherData;
+  }
   // TODO: Build parseCurrentWeather method
   // private parseCurrentWeather(response: any) {}
+  private parseCurrentWeather(response: any) {
+    const { name, main, weather } = response;
+    const currentWeather = new Weather(name, main.temp, weather[0].description);
+    return currentWeather;
+  }
   // TODO: Complete buildForecastArray method
   // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
+  private buildForecastArray(currentWeather: Weather, weatherData: any[]) {
+    const forecastArray = [];
+    for (let i = 1; i < weatherData.length; i++) {
+      const { dt_txt, main, weather } = weatherData[i];
+      const forecast = new Weather(currentWeather.city, main.temp, weather[0].description, dt_txt);
+      forecastArray.push(forecast);
+    }
+    return forecastArray;
+  }
   // TODO: Complete getWeatherForCity method
   // async getWeatherForCity(city: string) {}
+  async getWeatherForCity(city: string) {
+    this.cityName = city;
+    const coordinates = this.fetchAndDestructureLocationData();
+    const weatherData = this.fetchWeatherData(coordinates);
+    const currentWeather = this.parseCurrentWeather(weatherData);
+    const forecastArray = this.buildForecastArray(currentWeather, weatherData.list);
+    return { currentWeather, forecastArray };
+  }
 }
 
 export default new WeatherService();
